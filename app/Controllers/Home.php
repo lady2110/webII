f1ee6b1a800f87e2618794541f734e310de8e1f9
lady2110 <lady-2110@hotmail.com>
2021-09-29 20:59:27 -0500

    se creo una pagina de estilo de css

--- app/Controllers/Home.php
+++ app/Controllers/Home.php
@@ -9,7 +9,13 @@ class Home extends BaseController
         //pasar parametros a la vista
         $datos['Nombre']="Leidy";
         $datos['programas'] = ['Desarrollo de software','Sistemas','Redes','Video juegos'];
+        $datos['contacto']=[
+            'Cedula' => '1026144511',
+            'Nombre' => 'Leidy',
+            'Salario' => 150000
+        ];
         return view('welcome_message',$datos);
+        
     }
 
     function mindex(){
--- app/Views/welcome_message.php
+++ app/Views/welcome_message.php
@@ -9,7 +9,8 @@
 	<link rel="stylesheet" href="styles/bootstrap-reboot.min.css">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css">
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
-
+	<link rel="stylesheet" href="http://localhost/institucion_ci/public/assets/css/estilo1.css">
+	<link rel="stylesheet" href="<?php base_url()?>public/assets/css/estilo1.css"
 	
 </head>
 <body>
@@ -29,6 +30,16 @@
 			<td><?php echo $progs ?></td>
 		</tr><?php endforeach ?>
 	</table>
+	<ol>
+		<?php foreach($contacto as $cont){?>
+		<li><?php echo $cont?></li>
+		<?php }?>
+	</ol>
+	<span>
+			Cedula:<?=$contacto['Cedula']?>
+			Nombre:<?=$contacto['Nombre']?>
+			Salario:<?=$contacto['Salario']?>
+		</span>
 	<script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"
             integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj"
             crossorigin="anonymous"></script>
